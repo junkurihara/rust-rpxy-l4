145d3a5f1f465fb92117aac2ea246307debd6ccc
Jun Kurihara <junkurihara@users.noreply.github.com>
2026-03-03 16:57:02 +0900

    chore(refactor): update docker related files

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,7 @@ LOG_DIR=/rpxy-l4/log
 LOGGING=${LOG_TO_FILE:-false}
 
 # debug level logging
-if [ -z $LOG_LEVEL ]; then
+if [ -z "$LOG_LEVEL" ]; then
   LOG_LEVEL=info
 fi
 echo "rpxy-l4: Logging with level ${LOG_LEVEL}"
@@ -13,6 +13,6 @@ if "${LOGGING}"; then
   echo "rpxy-l4: Start with writing log files"
   RUST_LOG=${LOG_LEVEL} /rpxy-l4/bin/rpxy-l4 --config ${CONFIG_FILE} --log-dir ${LOG_DIR}
 else
-  echo "rpxy-4: Start without writing log files"
+  echo "rpxy-l4: Start without writing log files"
   RUST_LOG=${LOG_LEVEL} /rpxy-l4/bin/rpxy-l4 --config ${CONFIG_FILE}
 fi
